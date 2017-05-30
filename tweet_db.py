735e396434757c1a0b4b29e15de4bbb857703224
Henry Baldursson <henry.baldursson@gmail.com>
2017-05-30 20:56:58 +0000

    consistency

--- tweet_db.py
+++ tweet_db.py
@@ -15,23 +15,35 @@ tweet_list = [
         'time_tags': ['any'],
     },
     {
-        'tweet_templ': 'Góðir Íslendingar, nafnið {} er nú í boði.',
+        'tweet_templ': 'Góðir Íslendingar, nafnið „{}“ er nú í boði.',
         'time_tags': ['any'],
     },
     {
-        'tweet_templ': 'Mannanafnanefnd sefur aldrei. Hefuru íhugað nafnið {}?',
+        'tweet_templ': 'Mannanafnanefnd sefur aldrei. Hefuru íhugað nafnið „{}?“',
         'time_tags': ['night'],
     },
     {
-        'tweet_templ': 'Á morgunfundi Nefndarinnar var yfir vöfflum ákveðið að leyfa nafnið {}.',
+        'tweet_templ': 'Á morgunfundi Nefndarinnar var, yfir vöfflum, ákveðið að leyfa nafnið „{}.“',
         'time_tags': ['morning'],
     },
     {
-        'tweet_templ': 'Í aðeins takmarkaðan tíma erum við tilbúin að leyfa einhverjum heppnum að heita {}',
+        'tweet_templ': 'Í aðeins takmarkaðan tíma erum við tilbúin að leyfa einhverjum heppnum að heita „{}.“',
         'time_tags': ['afternoon', 'morning']
     },
     {
         'tweet_templ': 'Eitt erfiðasta verkefni dagsins var að samþykkja nafnið „{}.“',
         'time_tags': ['evening'],
-    }
+    },
+    {
+        'tweet_templ': 'Stundum ligg ég andvaka og hugsa um nafnið „{}.“',
+        'time_tags': ['evening', 'night'],
+    },
+    {
+        'tweet_templ': 'Inn á borð var að koma beiðni um nafnið „{}.“ Þetta verður eitthvað..',
+        'time_tags': ['morning', 'afternoon'],
+    },
+    {
+        'tweet_templ': 'Hvað á barnið að heita? Hvað með „{}.“',
+        'time_tags': ['morning', 'afternoon'],
+    },
 ]
