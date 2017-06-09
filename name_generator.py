0ee619d3bb8552a21ccccd25b47175b9a18d26cd
Henry Baldursson <henry.baldursson@gmail.com>
2017-06-09 00:32:38 +0000

    html changes on female page

--- name_generator.py
+++ name_generator.py
@@ -13,7 +13,7 @@ class NameGenerator(object):
         self.kvk_tree = html.fromstring(self.kvk_page.content)
 
         self.kk_nofn = self.kk_tree.xpath('//div[@id="mw-content-text"]/ul/li/a/text()')[0:-2]
-        self.kvk_nofn = self.kvk_tree.xpath('//div[@id="mw-content-text"]/ul/li/a/text()')[0:-2]
+        self.kvk_nofn = self.kvk_tree.xpath('//div[@id="mw-content-text"]/div/ul/li/a/text()')[0:-2]
 
         self.nofn = []
         self.nofn.append(self.kk_nofn)
